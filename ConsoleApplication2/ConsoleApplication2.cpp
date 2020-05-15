979e3cc62d82865b8d291c7a5f5283fe5ca48d1e
桐 孙 <2731881741@qq.com>
2020-05-15 14:38:38 +0800

    1

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,40 @@
+﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int main()
+{
+	float gamma = 0.45;
+	float LUT[256] = { 0 };
+	for (int i = 0; i < 256; i++) {
+		LUT[i] = pow(i * 1.0 / 255, gamma);
+		cout << LUT[i];
+	}
+	cv::Mat srcMat = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\第十二周\\face.jpg", 0);
+	for (int i = 0; i < srcMat.rows; i++)
+	{
+		for (int j = 0; j < srcMat.cols; j++)
+		{
+			srcMat.at<uchar>(i, j) = LUT[srcMat.at<uchar>(i, j)] * 255;
+		}
+	}
+
+	imshow("srcMat", srcMat);
+	waitKey(0);
+	return 0;
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
